d10db0e8d9644087a1c98de5c308c23a20d98261
happy-jinsu <68191793+happy-jinsu@users.noreply.github.com>
2021-10-13 23:54:07 +0900

    jump
    캐릭터 점프 및 이동 구현

---
+++ HW1_C093182_LSJ/Assets/Scenes/FollowCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject player;
+    public float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(x, y, z);
+    }
+}
---
+++ HW1_C093182_LSJ/Assets/Scenes/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    public int JumpPower;
+    public bool IsJumping;
+
+    // 처음 한번만 실행 되는 함수
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        IsJumping = false;
+    }
+    // 매 frame마다 실행 되는 함수
+    void Update()
+    {
+        Jump();
+
+        if (Input.GetKey(KeyCode.LeftArrow))  //왼쪽
+        {
+            if (IsJumping==false)
+            {
+                this.transform.Translate(-0.1f, 0.0f, 0.0f);
+            }
+
+            else
+            {
+                return;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))  //오른쪽
+        {
+            if (IsJumping == false)
+            {
+                this.transform.Translate(0.1f, 0.0f, 0.0f);
+            }
+
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void Jump()
+    {
+   
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!IsJumping)
+            {
+                IsJumping = true;
+                rigid.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            }
+
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = false;
+        }
+    }
+}
