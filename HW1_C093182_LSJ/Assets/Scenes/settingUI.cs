e5a63bc7495f3daa448cd5286df02f30418b1c1f
happy-jinsu <68191793+happy-jinsu@users.noreply.github.com>
2021-10-16 01:34:37 +0900

    타이틀 에셋 적용, Ui추가, 씬 여러 개 생성

---
+++ HW1_C093182_LSJ/Assets/Scenes/settingUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class settingUI : MonoBehaviour
+{
+    public GameObject Stop;
+    bool stoped = false;
+    void Start()
+    {
+        Stop.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("settingButton"))
+        {
+            stoped = !stoped;
+        }
+
+        if(stoped)
+        {
+            Stop.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if(!stoped)
+        {
+            Stop.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void End()
+    {
+        Application.Quit();
+    }
+}
