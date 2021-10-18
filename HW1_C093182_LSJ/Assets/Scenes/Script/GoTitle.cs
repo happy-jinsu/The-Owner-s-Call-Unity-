e6de78392efb4b094437e4da4708f6a6ba3fc110
happy-jinsu <68191793+happy-jinsu@users.noreply.github.com>
2021-10-18 21:33:29 +0900

    최종 밸런스 패치 (완)

---
+++ HW1_C093182_LSJ/Assets/Scenes/Script/GoTitle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoTitle : MonoBehaviour
+{
+    public void goTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
